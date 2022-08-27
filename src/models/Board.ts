fb8a70330b50dd9f42868348d36c29fc92f62bd0
tymphoto <9256187876@mail.ru>
2022-08-27 13:37:31 +0300

    board is ready

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,8 +1,11 @@
 import { Cell } from "./Cell";
 import { Colors } from "./Colors";
 import { Bishop } from "./figures/Bishop";
+import { King } from "./figures/King";
+import { Knigth } from "./figures/Knight";
 import { Pawn } from "./figures/Pawn";
 import { Queen } from "./figures/Queen";
+import { Rook } from "./figures/Rook";
 
 export class Board {
   cells: Cell[][] = []
@@ -26,9 +29,52 @@ export class Board {
     return this.cells[y][x]
   }
 
+  private addPawns() {
+    for(let i = 0; i < 8; i += 1){
+      new Pawn(Colors.BLACK, this.getCell(i, 1))
+      new Pawn(Colors.WHITE, this.getCell(i, 6))
+    }
+  }
+
+  private addKings() {
+    new King(Colors.BLACK, this.getCell(4, 0))
+    new King(Colors.WHITE, this.getCell(4, 7))
+  }
+
+  private addQueens(){
+    new Queen(Colors.BLACK, this.getCell(3, 0))
+    new Queen(Colors.WHITE, this.getCell(3, 7))
+  }
+
+  private addBishops() {
+    new Bishop(Colors.BLACK, this.getCell(2, 0))
+    new Bishop(Colors.BLACK, this.getCell(5, 0))
+    new Bishop(Colors.WHITE, this.getCell(2, 7))
+    new Bishop(Colors.WHITE, this.getCell(5, 7))
+  }
+
+  private addKnights() {
+    new Knigth(Colors.BLACK, this.getCell(1, 0))
+    new Knigth(Colors.BLACK, this.getCell(6, 0))
+    new Knigth(Colors.WHITE, this.getCell(1, 7))
+    new Knigth(Colors.WHITE, this.getCell(6, 7))
+  }
+
+  private addRooks(){
+    new Rook(Colors.BLACK, this.getCell(0, 0))
+    new Rook(Colors.BLACK, this.getCell(7, 0))
+    new Rook(Colors.WHITE, this.getCell(0, 7))
+    new Rook(Colors.WHITE, this.getCell(7, 7))
+  }
+
+  
+
   public addFigurs() {
-    new Queen(Colors.WHITE, this.getCell(3, 3))
-    new Pawn(Colors.BLACK, this.getCell(5, 6))
-    new Bishop(Colors.WHITE, this.getCell(2, 1))
+    this.addPawns()
+    this.addKings()
+    this.addQueens()
+    this.addBishops()
+    this.addKnights()
+    this.addRooks()
   }
 }
